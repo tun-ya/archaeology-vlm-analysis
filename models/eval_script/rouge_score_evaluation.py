172ed0b65b8a285f0370ce966743dd9a883fc095
tun-ya <tun_ya@outlook.com>
2024-11-21 19:02:54 +0400

    add rouge-L to evaluation code

--- models/eval_script/rouge_score_evaluation.py
+++ models/eval_script/rouge_score_evaluation.py
@@ -1,58 +1,39 @@
 import json
 import argparse
+import numpy as np
 from rouge_score import rouge_scorer
 
-# Example reference (ground truth) and hypothesis (generated text)
-reference = "The quick brown fox jumps over the lazy dog."
-hypothesis = "A fast brown fox jumped over a lazy dog."
-
 # Initialize the ROUGE scorer
-scorer = rouge_scorer.RougeScorer(['rouge1'], use_stemmer=True)
+scorer = rouge_scorer.RougeScorer(['rouge1', 'rougeL'], use_stemmer=True)
 
-
-def calculate_rouge1_score(prediction, ground_truth):
+def calculate_rouge_score(prediction, ground_truth):
     """
     Calculate the ROUGE_1 score between predicted and ground-truth answers.
 
     :param prediction: The predicted answer string.
     :param ground_truth: The ground truth (reference) answer string.
-    :return: ROUGE_1 score : precision, recall, fmeasure (floats between 0 and 1)
+    :return: ROUGE score : precision, recall, fmeasure (floats between 0 and 1)
     """
     
     # Compute the ROUGE scores
     scores = scorer.score(ground_truth, prediction)
-    for _, score in scores.items():
-        precision = score.precision
-        recall = score.recall
-        fmeasure = score.fmeasure
+    return scores
 
-    return precision, recall, fmeasure
-
-def evaluate_rouge1_on_multiple(data):
+def evaluate_rouge_on_multiple(data):
     """
     Evaluate ROUGE score on multiple data points and return the average score.
     
     :param data: A list of tuples, where each tuple contains a predicted answer and ground truth answer.
     :return: The average ROUGE score across all data points.
     """
-    f_total_score = 0
-    precision_total_score = 0
-    recall_total_score = 0
-
+    rouge_scores = []
     for item in result:
         prediction = item['pred_answer']
         ground_truth = item['answer']
-        precision_score, recall_score, f_score = calculate_rouge1_score(prediction, ground_truth)
-        f_total_score += f_score
-        precision_total_score += precision_score
-        recall_total_score += recall_score
+        scores = calculate_rouge_score(prediction, ground_truth)
+        rouge_scores.append(scores)
     
-    # Compute the average BLEU score
-    f_average_score = f_total_score / len(data) if data else 0
-    precision_average_score = precision_total_score / len(data) if data else 0
-    recall_average_score = recall_total_score / len(data) if data else 0
-
-    return f_average_score, precision_average_score, recall_average_score
+    return rouge_scores
 
 
 parser = argparse.ArgumentParser(description="Result file path argument")
@@ -66,9 +47,18 @@ with open(result_file_path, 'r') as file:
     result = json.load(file)
 
 # Evaluate score across multiple data points
-average_f, average_precision, average_recall = evaluate_rouge1_on_multiple(result)
+rouge_scores = evaluate_rouge_on_multiple(result)
+average_f_rouge = {metric: np.mean([score[metric].fmeasure for score in rouge_scores]) for metric in rouge_scores[0].keys()}
+average_precision_rouge = {metric: np.mean([score[metric].precision for score in rouge_scores]) for metric in rouge_scores[0].keys()}
+average_recall_rouge = {metric: np.mean([score[metric].recall for score in rouge_scores]) for metric in rouge_scores[0].keys()}
 
-print(f"Average ROUGE_1 F-measure score: {average_f:.4f}")
-print(f"Average ROUGE_1 Precision: {average_precision:.4f}")
-print(f"Average ROUGE_1 Recall: {average_recall:.4f}")
+
+# Print average ROUGE scores
+print("Average ROUGE scores:")
+for metric, avg_score in average_f_rouge.items():
+    print(f"{metric} F-measure: {avg_score:.4f}")
+for metric, avg_score in average_precision_rouge.items():
+    print(f"{metric} Precision: {avg_score:.4f}")
+for metric, avg_score in average_recall_rouge.items():
+    print(f"{metric} Recall: {avg_score:.4f}")
 
